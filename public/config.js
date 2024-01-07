65bee7900cb1b4c32d7e2f39b9ec1011d5b4aa0d
Maogxer <84484782+Maogxer@users.noreply.github.com>
2024-01-07 23:04:46 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,12 +2,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: '云生站点监测',
+  SiteName: '站点监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1078385-b9802a0868414b8a975d5038',
+    'ur2425493-c451cddce836659f250b1ba0',
   ],
 
     // 日志天数
@@ -19,18 +19,18 @@ window.Config = {
   ShowLink: true,
 
   // 导航栏菜单
-  Navi: [
-    {
-      text: '主页',
-      url: 'https://tjys.tk'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.qikaile.tk'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/qikaile/uptime-status'
-    },
-  ],
+ // Navi: [
+//    {
+//      text: '主页',
+//      url: 'https://tjys.tk'
+//    },
+//    {
+//      text: '博客',
+//      url: 'https://blog.qikaile.tk'
+//    },
+//    {
+ //     text: 'GitHub',
+//      url: 'https://github.com/qikaile/uptime-status'
+//    },
+//  ],
 };
