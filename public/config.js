a799e2a9a54a0f506dc4db55fc3c8b969544bee3
Maogxer <84484782+Maogxer@users.noreply.github.com>
2024-01-08 14:26:32 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,6 +4,8 @@ window.Config = {
   // 显示标题
   SiteName: '站点监测',
 
+  ApiDomain: 'https://worker-restless-meadow-58b1.yours-truly8126.workers.dev/',
+
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
